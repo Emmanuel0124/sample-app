e92cf8db560b55a5ca38ab619ae5eef6ecd7dcc7
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-27 12:56:44 -0500

    3 basic routes

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -2,4 +2,12 @@ class ExamplePagesController < ApplicationController
   def birds
     render json: {message: "Good-Day!"}
   end
+
+  def worm
+    render json: {message: "Good-bye!"}
+  end
+
+  def cat
+    render json: {message: "Nice Weather today!"}
+  end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get "/greeting", controller: "example_pages", action: "birds"
-  
+  get "/greet", controller: "example_pages", action: "birds"
+  get "/dissmis", controller: "example_pages", action: "worm"
+  get "/statment", controller: "example_pages", action: "cat"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  
   # Defines the root path route ("/")
   # root "articles#index"
-end
+end 
